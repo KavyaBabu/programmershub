527f83c6b739cd6f316baac21df7b43d72dec453
kavyababu <kavyababu7@gmail.com>
2019-04-24 00:01:35 +0530

    Added target folder to gitignore

---
+++ src/main/java/com/kb/dp/subsequence/LCSMemoisation.java
@@ -0,0 +1,22 @@
+package com.kb.dp.subsequence;
+
+public class LCSMemoisation {
+    public int getLCS(String word1, String word2) {
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+        int[][] lookupMatrix = new int[word1Length +1][word2Length +1];
+        for (int i = 0; i <= word1Length; i++) {
+            for (int j = 0; j <= word2Length; j++) {
+                if(i==0 || j==0){
+                    lookupMatrix[i][j] = 0;
+                } else if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    lookupMatrix[i][j] = lookupMatrix[i-1][j-1] + 1 ;
+                } else {
+                    lookupMatrix[i][j] = Integer.max(lookupMatrix[i][j-1], lookupMatrix[i-1][j]);
+                }
+            }
+        }
+
+        return lookupMatrix[word1Length][word2Length];
+    }
+}
---
+++ src/test/java/com/kb/dp/subsequence/LCSMemoisationTest.java
@@ -0,0 +1,43 @@
+package com.kb.dp.subsequence;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LCSMemoisationTest {
+
+    @Test
+    @DisplayName("Returns the length of longest common substring between 2 strings")
+    public void returnLengthOfLCSForTwoStrings(){
+        LCSMemoisation lcsMemoisation = new LCSMemoisation();
+        String word1 = "abcde";
+        String word2 = "dfe";
+        int lcsLEngth = lcsMemoisation.getLCS(word1, word2);
+        assertEquals(lcsLEngth, 2);
+
+    }
+
+    @Test
+    @DisplayName("Returns the length of longest common substring between a string and an empty string")
+    public void returnLengthOfLCSForEmptyStrings(){
+        LCSMemoisation lcsMemoisation = new LCSMemoisation();
+        String word1 = "abcde";
+        String word2 = "";
+        int lcsLEngth = lcsMemoisation.getLCS(word1, word2);
+        assertEquals(lcsLEngth, 0);
+
+    }
+
+
+    @Test
+    @DisplayName("Returns the length of longest common substring between a string and a string with repeating char")
+    public void returnLengthOfLCSForStringWithRepeatingChar(){
+        LCSMemoisation lcsMemoisation = new LCSMemoisation();
+        String word1 = "abcde";
+        String word2 = "abcc";
+        int lcsLEngth = lcsMemoisation.getLCS(word1, word2);
+        assertEquals(lcsLEngth, 3);
+
+    }
+}
